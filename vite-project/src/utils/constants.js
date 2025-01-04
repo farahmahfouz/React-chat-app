412778a7468adf45c82deeb40a9021e0c7d4a4c3
Farah Mahfouz <122357697+farahmahfouz@users.noreply.github.com>
2025-01-05 00:31:29 +0200

    Update constants.js

--- vite-project/src/utils/constants.js
+++ vite-project/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = import.meta.env.VITE_GLITCH_URL;
+export const HOST = https://sphenoid-azure-oviraptor.glitch.me/
 
 export const AUTH_ROUTES = "api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
